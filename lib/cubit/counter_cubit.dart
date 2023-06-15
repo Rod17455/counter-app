35b7de50b8feeb60024ed5b2b5854870b1d89d41
Rodrigo Tellez <rtellez476@gmail.com>
2023-06-14 23:16:27 -0600

    Descripción del cambio

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    if (state < 10) {
+      emit(state + 1);
+    } else {
+      // Se ha alcanzado el límite de 10.
+       Fluttertoast.showToast(
+          msg: "Ya ha alcanzo el límite",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM // Also possible "TOP" and "CENTER"
+        );
+    }
+  }
+
+  void decrement() {
+    if (state > -10) {
+      emit(state - 1);
+    } else {
+      // Se ha alcanzado el límite de -10.
+      Fluttertoast.showToast(
+          msg: "Ya ha alcanzo el límite",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM // Also possible "TOP" and "CENTER"
+        );
+    }
+  }
+}
